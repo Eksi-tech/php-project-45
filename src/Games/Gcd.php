0e13c399437932f225ccdea29e65f60ddefb513b
Viktor Safonov <viktor.safonov1223@gmail.com>
2025-11-09 16:08:46 +0300

    missed 1 random_int in src/Games/Gcd.php

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -7,7 +7,7 @@ function getRules(): string
 
 function generateQuestion(): string
 {
-    if (rand(1, 3) === 1) {
+    if (random_int(1, 3) === 1) {
         $base = random_int(2, 25);
         $firstNumber = $base * random_int(2, 10);
         $secondNumber = $base * random_int(2, 10);
