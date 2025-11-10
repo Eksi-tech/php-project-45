9c8a956b0d191f8cf42474f658c3880291d4ae54
Viktor Safonov <viktor.safonov1223@gmail.com>
2025-11-10 20:29:26 +0300

    fixed even with 'clean' function

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -6,11 +6,17 @@ use function BrainGames\Engine\runEngine;
 
 const RULES = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+function isEven(int $number): bool
+{
+    return $number % 2 === 0;
+}
+
 function runGame(): void
 {
     $generateData = function () {
         $number = random_int(1, 100);
-        $isEven = $number % 2 === 0;
+        
+        $isEven = isEven($number);
 
         return [
             'question' => (string)$number,
